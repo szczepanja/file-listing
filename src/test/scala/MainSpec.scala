96ea4f866d80e8a0612e5fbae256fbf8a15f97a5
a_szczepanik <a.szczepanik@adform.com>
2022-05-07 11:28:38 +0200

    Upgrade tests

--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -1,19 +1,34 @@
-import Main._
 import org.scalatest._
 import flatspec._
 import matchers._
 
 class MainSpec extends AnyFlatSpec with should.Matchers {
 
-  "Main.getFileFromDir" should "return empty List if no directory was provided" in {
-    getFileFromDir("") shouldBe List()
-    List.empty shouldBe empty
+  val parser: Main.type = Main
+
+  val directory = "C:\\Users\\a.szczepanik\\sandbox\\scala-academy-sandbox\\anja\\file-listing\\src"
+  val main = "C:\\Users\\a.szczepanik\\sandbox\\scala-academy-sandbox\\anja\\file-listing\\src\\main"
+  val test = "C:\\Users\\a.szczepanik\\sandbox\\scala-academy-sandbox\\anja\\file-listing\\src\\test"
+  val actual: Seq[String] = parser.getFileFromDir(directory).map((el) => el.toString)
+
+  it should "check if file exist in result" in {
+    actual should contain(main)
+    actual should contain(test)
+  }
+
+  it should "return empty Seq if no directory was provided" in {
+    parser.getFileFromDir("") shouldBe Seq()
+    Seq.empty shouldBe empty
   }
 
   it should "let us check the size of collections" in {
     val listOfFiles = List("build.sbt", "Main", "MainSpec")
     listOfFiles should have size 3
     listOfFiles should not be empty
+  }
+
+  it should "return sequence of files contained in directory" in {
+    actual shouldBe Seq(main, test)
   }
 
 }
